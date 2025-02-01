7713f20e17136ffa6ed7bae9216419a851c38281
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-02-01 15:02:53 +0000

    Fix variable declaration in getGuess()

--- scripts/script.js
+++ scripts/script.js
@@ -16,8 +16,8 @@ function playGame() {
   }
 
   function getGuess() {
-    row = prompt(`${currentPlayer}, please enter the row in which you want to place your token. (row 0, 1, or 2)`);
-    column = prompt(`${currentPlayer}, please enter the column in which you want to place your token. (column 0, 1, or 2)`);
+    const row = prompt(`${currentPlayer}, please enter the row in which you want to place your token. (row 0, 1, or 2)`);
+    const column = prompt(`${currentPlayer}, please enter the column in which you want to place your token. (column 0, 1, or 2)`);
 
     if(board.isFieldEmpty(row, column)) {
       return [row, column];
